5734889e46ec3a32bb3fd0fccc8d0683c764afa3
Mir Immad <mirimmad17@gmail.com>
2020-10-24 22:48:12 +0530

    parser add regex and or

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
-	"github.com/baseline/ast"
+	//"github.com/baseline/ast"
+	"github.com/baseline/parser"
 )
 
 func main() {
 
-	call := ast.Call{"Fun", []ast.AST{&ast.Number{14}}}
-	fmt.Println(call)
+	//call := ast.Call{"Fun", []ast.AST{&ast.Number{14}}}
+	s := parser.Source{"yello World", 0}
+	p := parser.NewParser()
+	fmt.Println(p.Regex("hello").Or(p.Regex("Hello")).Parse(s))
 
 }
--- parser/parser.go
+++ parser/parser.go
@@ -5,9 +5,11 @@ import (
 	"regexp"
 )
 
+type ParseFn func(s Source) *ParserResult
+
 type Source struct {
-	str   string
-	index int
+	Str   string
+	Index int
 }
 
 type ParserResult struct {
@@ -15,16 +17,57 @@ type ParserResult struct {
 	source Source
 }
 
-func (s *Source) match(pattern string) (*ParserResult, error) {
+func (s *Source) Match(pattern string) (*ParserResult, error) {
 	r, err := regexp.Compile(pattern)
 	if err != nil {
 		return nil, errors.New("Regex Error")
 	}
-	match_ := r.FindStringIndex(s.str[s.index:])
+	match_ := r.FindStringIndex(s.Str[s.Index:])
 	if len(match_) == 0 {
 		return nil, errors.New("No Match")
 	}
-	newIndex := s.index + (match_[1] - match_[0])
-	source := Source{s.str, newIndex}
+	newIndex := s.Index + (match_[1] - match_[0])
+	source := Source{s.Str, newIndex}
 	return &ParserResult{match_, source}, nil
 }
+
+type Parser struct {
+	Parse ParseFn
+}
+
+func NewParser() *Parser {
+	return &Parser{}
+}
+
+func (p *Parser) Regex(pattern string) *Parser {
+	return &Parser{func(s Source) *ParserResult {
+		r, err := s.Match(pattern)
+		if err != nil {
+			return nil
+		}
+		return r
+	}}
+}
+
+func (p *Parser) Constant(value interface{}) *Parser {
+	return &Parser{func(s Source) *ParserResult {
+		return &ParserResult{value, s}
+	}}
+}
+
+func (p1 *Parser) Or(p2 *Parser) *Parser {
+	return &Parser{func(s Source) *ParserResult {
+		result := p1.Parse(s)
+		if result != nil {
+			return result
+		}
+		return p2.Parse(s)
+	}}
+}
+
+// func (t *Parser) ZeroOrMore(p *Parser) *Parser {
+// 	return &Parser{func(s Source) *ParserResult {
+// 		results := make([]ParserResult, 0)
+
+// 	}}
+// }
